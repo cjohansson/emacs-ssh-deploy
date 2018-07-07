9c3f519d0d501b0330c5fe7eddab4f5e7552b513
Christian Johansson <christian@cvj.se>
2018-07-07 08:24:24 +0200

    Nicer looking mode-line with square brackets

--- ssh-deploy.el
+++ ssh-deploy.el
@@ -347,8 +347,8 @@
 (defun ssh-deploy--mode-line-status-text-format (text)
   "Return a formatted string based on TEXT."
   (if (string= text "")
-      (format " DPL ")
-    (format " DPL: %s " text)))
+      (format " [DPL] ")
+    (format " [DPL:%s] " text)))
 
 (defun ssh-deploy--insert-keyword (text)
   "Insert TEXT as bold text."
