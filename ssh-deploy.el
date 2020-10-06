cc91b56a7f07ae63fcf78a040001491d040ec9ba
Christian Johansson <christian@cvj.se>
2020-10-06 14:13:54 +0200

    Updated README and version

--- ssh-deploy.el
+++ ssh-deploy.el
@@ -5,8 +5,8 @@
 ;; Author: Christian Johansson <christian@cvj.se>
 ;; Maintainer: Christian Johansson <christian@cvj.se>
 ;; Created: 5 Jul 2016
-;; Modified: 6 Mar 2020
-;; Version: 3.1.11
+;; Modified: 10 Oct 2020
+;; Version: 3.1.12
 ;; Keywords: tools, convenience
 ;; URL: https://github.com/cjohansson/emacs-ssh-deploy
 
